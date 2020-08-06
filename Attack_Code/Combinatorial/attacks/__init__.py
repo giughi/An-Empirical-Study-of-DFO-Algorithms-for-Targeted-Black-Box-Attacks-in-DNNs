f94b9c77b9304709fa6b94a0e3d1cad44f5908d7
giughi <32544670+giughi@users.noreply.github.com>
2020-08-06 16:33:45 +0100

    Update __init__.py

--- Attack_Code/Combinatorial/attacks/__init__.py
+++ Attack_Code/Combinatorial/attacks/__init__.py
@@ -1,4 +1,1 @@
-# from Attack_Code.Combinatorial.attacks.parsimonious_attack import ParsimoniousAttack
 
-from Attack_Code.Combinatorial.attacks.parsimonious_attack_2 import ParsimoniousAttack_2
-from Attack_Code.Combinatorial.attacks.parsimonious_attack_3_restarts import ParsimoniousAttack_restarts
